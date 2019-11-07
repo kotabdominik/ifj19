453f844bfdd6c56faf8f0091596e374da6130eaa
Dominiček Kimodíček <dominik.kotab@gmail.com>
2019-11-07 23:19:10 +0100

    create error.h, scanner.h, strings.c, strings.h

---
+++ strings.h
@@ -0,0 +1,24 @@
+/**
+  * strings.h
+  * Project IFJ19
+  *
+  * Březina Jindřich (xbrezi21@stud.fit.vutbr.cz)
+  * Gumančík Pavol (xguman01@stud.fit.vutbr.cz)
+  * Kotáb Dominik (xkotab01@stud.fit.vutbr.cz)
+  * Moravčík Tomáš (xmorav41@stud.fit.vutbr.cz)
+  *
+  * Brief: operations for strings
+  *
+  */
+
+typedef struct {
+  char *string;
+  int length;
+  int allocatedSize;
+} smartString;
+
+void stringInit(smartString *str);
+void stringFree(smartString *str);
+void stringClear(smartString *str);
+void stringAddChar(smartString *str, char c);
+int stringIsKeyword(smartString *str);
