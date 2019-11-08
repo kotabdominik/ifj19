d149376f568a4a1a15529bb488af602f4c7f5d3c
Bigani <geeezwhyme@gmail.com>
2019-11-08 17:20:25 +0100

    ; to , pepo

--- scanner.h
+++ scanner.h
@@ -17,36 +17,36 @@
   #include "strings.h"
 
   typedef enum {
-    DEF;
-    ELSE;
-    IF;
-    NONE;
-    PASS;
-    RETURN;
-    WHILE;
+    DEF,
+    ELSE,
+    IF,
+    NONE,
+    PASS,
+    RETURN,
+    WHILE,
   } keyword;
 
   typedef enum {
-    PLUS; //+
-    MINUS; //-
-    TIMES; //*
-    INT; //integer
-    FLOAT; //float
-    STR; //string
-    DIVFLOAT; //úplné dělení do floatu
-    DIV; //celočíselné dělení, očekává dva int
-    LESS; //<
-    GREATER; //>
-    LESSEQ; //<=
-    GREATEREQ; //>=
-    EQ; //==
-    NOTEQ; //!=
-    EOL; //konec radku
-    EOFTOKEN; //konec souboru
-    LEFTBRACKET; // (
-    RIGHTBRACKET; // )
-    COMMA; // ,
-    SEMICOLON; // ;
+    PLUS, //+
+    MINUS, //-
+    TIMES, //*
+    INT, //integer
+    FLOAT, //float
+    STR, //string
+    DIVFLOAT, //úplné dělení do floatu
+    DIV, //celočíselné dělení, očekává dva int
+    LESS, //<
+    GREATER, //>
+    LESSEQ, //<=
+    GREATEREQ, //>=
+    EQ, //==
+    NOTEQ, //!=
+    EOL, //konec radku
+    EOFTOKEN, //konec souboru
+    LEFTBRACKET, // (
+    RIGHTBRACKET, // )
+    COMMA, // ,
+    SEMICOLON, // ;
   } tokenType;
 
   typedef struct {
