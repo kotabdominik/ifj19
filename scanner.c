decc0b4b75563365daba93e81e13ad6cee8ac8fc
Dominiček Kimodíček <dominik.kotab@gmail.com>
2019-11-08 16:29:45 +0100

    gl tome

---
+++ scanner.c
@@ -0,0 +1,23 @@
+/**
+  * scanner.c
+  * Project IFJ19
+  *
+  * Březina Jindřich (xbrezi21@stud.fit.vutbr.cz)
+  * Gumančík Pavol (xguman01@stud.fit.vutbr.cz)
+  * Kotáb Dominik (xkotab01@stud.fit.vutbr.cz)
+  * Moravčík Tomáš (xmorav41@stud.fit.vutbr.cz)
+  *
+  * Brief: scanner and Lexical analyzer
+  *
+  */
+
+#include "scanner.h"
+
+
+token nextToken() {
+  token token;
+  tokenAttribute attribute;
+
+  stringInit(&token.string);
+
+}
