07a60bb6bd40e749c0a58f2ab3e0224ee3483353
Dominiček Kimodíček <dominik.kotab@gmail.com>
2019-11-09 08:08:29 +0100

    addnul jsem struktury variables a funkcí, inicializace a nějaký examples

--- symtable.c
+++ symtable.c
@@ -11,22 +11,60 @@
   *
   */
 
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include "symtable.h"
+
 /*
  * inicializace symbol table
  * table je implementovan pomoci binarniho stromu
  * = inicializuje se binarni strom
 */
-void initSymbolTable(symbolTable *sT){
-  /*strominit*/(&(sT->root));
+void initSymbolTable(symbolTable *sT, unsigned int size){
+  sT = (symbolTable *) malloc(sizeof(*sT) + size * sizeof(sT->symtabList[0]));
+  if (!sT) {
+    printf("%s\n", "yikes");
+  }
+  sT->size = size;
+  for (unsigned int i = 0; i < sT->size; i++) {
+		sT->symtabList[i] = NULL;
+	}
 }
 
-/*
- * vlozeni dat o funkci do symbol table
- *
-*/
-void insertFunctionSymbolTable(symbolTable *sT, smartString string){
+void freeSymbolTable(symbolTable *sT) {
+  symtableItem *tmpItem;
 
+  for (unsigned int i = 0; i < sT->size; i++) {
+    tmpItem = sT->symtabList[i];
+    while (tmpItem) {
+      symtableItem *thisItem = tmpItem;
+      tmpItem = tmpItem->next;
+      //xddd vy si fakt myslite ze to budu freeovat yeet (jk todo ... unless *blushes*)
+      break;
+    }
+  }
+}
+
+void main() {
+  symbolTable *table;
+  initSymbolTable(table, 1);
+  symtableItem *tableItem;
+  tableItem = (symtableItem *) malloc(sizeof(*tableItem));
+  if (!tableItem) {
+    printf("%s\n", "yeet :c");
+  }
+
+  tableItem->elementType.variable.value.INT = 5; //setování value int tableItemu
+
+  tableItem->key = (char *) malloc(sizeof(char) * 5); //malokace a setování klíče
+  strcpy(tableItem->key, "aaaa\0");
+
+  table->symtabList[0] = tableItem; //přiřazování tableItemu do tablu
+
+  printf("%d\n", table->symtabList[0]->elementType.variable.value.INT);
+  printf("%s\n", table->symtabList[0]->key);
 }
 
 
-/* for full version go to https://github.com/DavidHribek/IFJ/blob/development/src/symtable.c */
+/* for full version go to https://github.com/Endrych/IFJ-Projekt/blob/master/src/symtable.c */
--- symtable.h
+++ symtable.h
@@ -11,17 +11,23 @@
   *
   */
 
-#include "string.h"
+#include <stdbool.h>
+#include "strings.h"
 
 #define MAX_SYMTABLE_SIZE 17569 //nějaká meme hodnota, která je shodou okolností prvočíslo
 
 #ifndef _SYMBOLTABLE_H_
 #define _SYMBOLTABLE_H_
 
-/* chybi nejake struktury */
+//init value
+typedef union{
+    char *string;
+    int INT;
+    double FLOAT;
+    bool BOOL;
+} value;
 
-typedef enum //možné datové typy - 10.3 v zadání
-{
+typedef enum { //možné datové typy - 10.3 v zadání
 	UNDEFINED, //nvm jestli nutný
 	INT,
 	BOOL,
@@ -29,17 +35,53 @@ typedef enum //možné datové typy - 10.3 v zadání
 	STRING,
 } dataType;
 
-typedef struct {
-  root; //tady bude ukazatel na koren(nejaky node) binarniho stromu
+typedef enum {
+  FUNCTION,
+	VARIABLE,
+} elementType;
+
+typedef struct { //typedef struct var_item{
+	bool declared;
+	//bool used;
+	value value;
+  //char *key;
+	dataType type;
+} variableData;
+
+typedef struct { //typedef struct fc_item{
+	bool declared; //je zeloženo
+	bool defined; //je poprvé přiřazena hodnota
+	dataType returnType;
+	int argCount;
+	//struct symtab_item *arguments;
+	//struct symtable *sym_table;
+	//char *key;
+} functionData;
+
+typedef struct symtableItm {
+	struct symtableItm *next;
+	char *key;
+	union {
+		functionData function;
+		variableData variable;
+	} elementType;
+	elementType type;
+} symtableItem;
+
+typedef struct symtable {
+	unsigned int size;
+	symtableItem *symtabList[];
 } symbolTable;
 
 // Symbol table
-typedef Sym_table_item* Sym_table[MAX_SYMTABLE_SIZE];
+//typedef Sym_table_item* Sym_table[MAX_SYMTABLE_SIZE]; nejspíš neřešit
 
-void initSymbolTable(symbolTable *sT); //inicializace symbol tablu
+void initSymbolTable(symbolTable *sT, unsigned int size); //inicializace symbol tablu
+void freeSymbolTable(symbolTable *sT); //uvolnění celého symbol tablu
+
 void insertFunctionSymbolTable(symbolTable *sT, smartString string); //prida data o funkci do symbol tablu
 void insertVariableSymbolTable(symbolTable *sT, smartString string); //prida data o promenne do symbol tablu
-/* zatim neznamy node */ searchSymbolTable(symbolTable *sT, smartString string); //vyhleda prvek v symbol tablu
+/* zatim neznamy node */ //searchSymbolTable(symbolTable *sT, smartString string); //vyhleda prvek v symbol tablu
 void removeSymbolTable(symbolTable *sT, smartString string); //odstrani prvek ze symbol tablu
 void deleteSymbolTable(symbolTable *sT); //odstrani cely symbol table
 /* vlozeni builtIn funkci do symtablu ???? */
